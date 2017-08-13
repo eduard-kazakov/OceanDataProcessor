238bc532690b4c25a03853068dac483e8b2a0be1
Eduard Kazakov <silenteddie@gmail.com>
2017-08-14 02:38:18 +0300

    GLODAP data support

--- ODPTools.py
+++ ODPTools.py
@@ -13,8 +13,8 @@ def lon_lat_to_grid_cell (lon,lat,xmin,xmax,ymin,ymax,xstep,ystep,proj_desc):
     
     x,y = lon_lat_to_x_y(lon,lat,proj_desc)
     if (x<xmin) or (x>xmax) or (y<ymin) or (y>ymax):
-        print 'point not in grid'
-        return -1, -1
+        #print 'point not in grid'
+        return {'row':-1,'col':-1}
     col = int(((x-xmin)/xstep))
     row = size_y - int((y-ymin)/ystep) - 1
     return {'row':row,'col':col}
--- OceanDataProcessor.py
+++ OceanDataProcessor.py
@@ -2,17 +2,20 @@
 
 from __future__ import print_function
 import time
+import ODPTools
+
 
 class OceanDataProcessor():
     
     NODATA = '-999'
     DEPTH_LIMIT = 50
-    supported_data_types = ['WOD']
+    supported_data_types = ['WOD','GLODAP']
+    base_titles = ['name','lat','lon','year,','month','day']
     
     projection = '+proj=laea +lat_0=90 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs'
     grid = {'xmin':-5000000,'xmax':5000000, 'ymin':-5000000, 'ymax': 5000000, 'xstep': 4000, 'ystep': 4000}
     
-    def __init__(self, data_type, data_source, delimeter, year_min, year_max, lat_min, lat_max, lon_min, lon_max):
+    def __init__(self, data_type, data_source, delimeter, year_min, year_max, lat_min, lat_max, lon_min, lon_max, extra=None):
         if data_type not in self.supported_data_types:
             print ('Data type is not supported')
             return -1
@@ -26,6 +29,7 @@ class OceanDataProcessor():
         self.lat_max = lat_max
         self.lon_min = lon_min
         self.lon_max = lon_max
+        self.extra = extra
         
     ######### Parsers
     
@@ -36,29 +40,48 @@ class OceanDataProcessor():
         return line[:-len(delimeter)]
     
     def WOD_station_to_lines (self, WOD_station, uniqie_variables, delimeter):
-        #print ('asd'    )
-        
         depth_index = WOD_station['variables'].index('Depth')
         lines_array = []
         if depth_index >= 0:
             for measurement in WOD_station['measurements']:
                 if float(measurement[depth_index]) > self.DEPTH_LIMIT:
                     continue
-                current_line_array = [WOD_station['name'],WOD_station['lat'],WOD_station['lon'],WOD_station['year'],WOD_station['month'],WOD_station['day']]
+                current_line_array = [WOD_station['name'],WOD_station['lat'],WOD_station['lon'],WOD_station['year'],WOD_station['month'],WOD_station['day'],WOD_station['row'],WOD_station['col']]
                 for unique_variable in uniqie_variables:
                     current_line_array.append(self.NODATA)
                 i = 0
                 while i < len(measurement):
                     current_parameter_name = WOD_station['variables'][i]
+                        
                     current_parameter_index = uniqie_variables.index(current_parameter_name)
                     if measurement[i].isdigit():
-                        current_line_array[current_parameter_index + 6] = measurement[i]
+                        current_line_array[current_parameter_index + 8] = measurement[i]
                     else:
-                        current_line_array[current_parameter_index + 6] = measurement[i]
+                        current_line_array[current_parameter_index + 8] = measurement[i]
                     i += 1
                 lines_array.append(self.list_to_line(current_line_array,delimeter))
 
             return lines_array
+            
+    def GLODAP_station_to_lines (self, GLODAP_station, uniqie_variables, delimeter):
+        
+        lines_array = []
+        current_line_array = [GLODAP_station['name'],GLODAP_station['lat'],GLODAP_station['lon'],GLODAP_station['year'],GLODAP_station['month'],GLODAP_station['day'],GLODAP_station['row'],GLODAP_station['col']]
+        for unique_variable in uniqie_variables:
+            current_line_array.append(self.NODATA)
+        i = 0
+        while i < len(GLODAP_station['measurements']):
+            #print (i)
+            current_parameter_name = GLODAP_station['variables'][i]
+            current_parameter_index = uniqie_variables.index(current_parameter_name)
+            if GLODAP_station['measurements'][i].isdigit():
+                current_line_array[current_parameter_index + 8] = GLODAP_station['measurements'][i]
+            else:
+                current_line_array[current_parameter_index + 8] = GLODAP_station['measurements'][i]
+            i += 1
+        lines_array.append(self.list_to_line(current_line_array,delimeter))
+
+        return lines_array
                 
     def parse_WOD_point_data(self, data_source):
         input_data = open(data_source,'r').readlines()
@@ -66,7 +89,7 @@ class OceanDataProcessor():
         stations = []
         unique_variables = []
         station_number = 0        
-        current_station = {'name':'','lat':'','lon':'','year':'','month':'','day':'','variables':[],'measurements':[]}
+        current_station = {'name':'','lat':'','lon':'','year':'','month':'','day':'','row':'','col':'','variables':[],'measurements':[]}
         for line in input_data:
             if station_parsing:
                 current_line = line.replace(' ','').split(self.delimeter)
@@ -114,16 +137,99 @@ class OceanDataProcessor():
                     if (float(current_station['year']) >= self.year_min) and (float(current_station['year']) <= self.year_max):
                         if (float(current_station['lat']) >= self.lat_min) and (float(current_station['lat']) <= self.lat_max):
                             if (float(current_station['lon']) >= self.lon_min) and (float(current_station['lon']) <= self.lon_max):
+                                
+                                ### grid
+                                grid_cell = ODPTools.lon_lat_to_grid_cell(current_station['lon'],current_station['lat'],
+                                                                                         self.grid['xmin'],self.grid['xmax'],
+                                                                                         self.grid['ymin'],self.grid['ymax'],
+                                                                                         self.grid['xstep'],self.grid['ystep'],
+                                                                                         self.projection)
+                                
+                                current_station['row'] = grid_cell['row']
+                                current_station['col'] = grid_cell['col']
+                                ###
+
                                 station_number += 1
                                 print('Processing raw record #%s' % str(station_number), end='\r')
                                 stations.append(current_station)
                     
-                current_station = {'name':'','lat':'','lon':'','year':'','month':'','day':'','variables':[],'measurements':[]}
+                current_station = {'name':'','lat':'','lon':'','year':'','month':'','day':'','row':'','col':'','variables':[],'measurements':[]}
                     
                 station_parsing = True
                 
         print ('')
         return {'stations':stations,'unique_variables':unique_variables}
+
+
+    def parse_GLODAP_point_data(self, data_source):
+        input_data = open(data_source,'r').readlines()
+        titles = input_data[0].split(',')
+        stations = []
+        i = 1
+        if self.extra:
+            try:
+                allowed_titles = self.extra['allowed_titles']
+            except:
+                allowed_titles = titles
+        else:
+            allowed_titles = titles
+            
+        indicies = {'name':titles.index('station'),'lat':titles.index('latitude'),
+                    'lon':titles.index('longitude'),'year':titles.index('year'),
+                    'month':titles.index('month'),'day':titles.index('day'),
+                    'depth':titles.index('depth')}
+
+        variables=[]
+        for title in titles:
+            if (title not in self.base_titles) and (title in allowed_titles):
+                variables.append(title)
+                
+        print (variables)
+            
+        while i < len(input_data):
+            current_station = {'name':'','lat':'','lon':'','year':'','month':'','day':'','row':'','col':'','variables':variables,'measurements':[]}
+            
+            current_data = input_data[i].split(',')
+            
+            if float(current_data[indicies['depth']]) > self.DEPTH_LIMIT:
+                
+                #print (current_data[indicies['depth']])
+                i+=1
+                continue
+            
+            current_station['name'] = current_data[indicies['name']]
+            current_station['lat'] = current_data[indicies['lat']]
+            current_station['lon'] = current_data[indicies['lon']]
+            current_station['year'] = current_data[indicies['year']]
+            current_station['month'] = current_data[indicies['month']]
+            current_station['day'] = current_data[indicies['day']]
+
+            for index, data_record in enumerate(current_data):
+                data_record_title = titles[index]
+                if data_record_title in current_station['variables']:
+                    current_station['measurements'].append(data_record)
+
+            if (float(current_station['year']) >= self.year_min) and (float(current_station['year']) <= self.year_max):
+                        if (float(current_station['lat']) >= self.lat_min) and (float(current_station['lat']) <= self.lat_max):
+                            if (float(current_station['lon']) >= self.lon_min) and (float(current_station['lon']) <= self.lon_max):
+
+                                grid_cell = ODPTools.lon_lat_to_grid_cell(current_station['lon'],current_station['lat'],
+                                                                          self.grid['xmin'],self.grid['xmax'],
+                                                                          self.grid['ymin'],self.grid['ymax'],
+                                                                          self.grid['xstep'],self.grid['ystep'],
+                                                                          self.projection)
+
+                                current_station['row'] = grid_cell['row']
+                                current_station['col'] = grid_cell['col']
+
+                                print('Processing raw record #%s' % str(i), end='\r')
+                                stations.append(current_station)
+            
+            i += 1
+
+        return {'stations':stations,'unique_variables':variables}
+
+
     #################
         
         
@@ -133,9 +239,10 @@ class OceanDataProcessor():
             dataset = self.parse_WOD_point_data(self.data_source)
             return dataset
         
-    
-    def parse_and_write_to_file(self):
-        pass
+        if self.data_type == 'GLODAP':
+            dataset = self.parse_GLODAP_point_data(self.data_source)
+            return dataset
+        
     
     def write_to_file(self, dataset, output_file):
         if self.data_type == 'WOD':
@@ -143,12 +250,14 @@ class OceanDataProcessor():
             #return dataset
             output = open(output_file,'w')
             unique_variables = dataset['unique_variables']
-            title_line = 'NAME' + self.delimeter + 'LAT' + self.delimeter + 'LON' + self.delimeter + 'YEAR' + self.delimeter + 'MONTH' + self.delimeter + 'DAY' + self.delimeter
+            title_line = 'NAME' + self.delimeter + 'LAT' + self.delimeter + 'LON' + self.delimeter + 'YEAR' + self.delimeter + 'MONTH' + self.delimeter + 'DAY' + self.delimeter + 'ROW' + self.delimeter + 'COL' + self.delimeter
             for unique_variable in unique_variables:
                 title_line += unique_variable + self.delimeter
             
             output.write(title_line + '\n')
             station_number = 0
+            
+            
             for station in dataset['stations']:
                 station_number += 1
                 print('Writing station #%s' % str(station_number), end='\r')
@@ -157,9 +266,32 @@ class OceanDataProcessor():
                     output.write(line+'\n')
     
             output.close()
+            
+        if self.data_type == 'GLODAP':
+            print ('')
+            output = open(output_file,'w')
+            unique_variables = dataset['unique_variables']
+            title_line = 'NAME' + self.delimeter + 'LAT' + self.delimeter + 'LON' + self.delimeter + 'YEAR' + self.delimeter + 'MONTH' + self.delimeter + 'DAY' + self.delimeter + 'ROW' + self.delimeter + 'COL' + self.delimeter
+            for unique_variable in unique_variables:
+                title_line += unique_variable + self.delimeter
+            
+            output.write(title_line + '\n')
+            station_number = 0
+            for station in dataset['stations']:
+                station_number += 1
+                print('Writing station #%s' % str(station_number), end='\r')
+                lines_array = self.GLODAP_station_to_lines(station,unique_variables,self.delimeter)
+                for line in lines_array:
+                    output.write(line+'\n')
+    
+            output.close()
     
 start_time = time.time()
-WOD_sample = OceanDataProcessor('WOD','E:/OCOLOR/data_examples/point_data_samples/ocldb1490697552.28643.APB.csv',',',1997, 2018, 40, 90, -180, 180)
-var = WOD_sample.parse()
-WOD_sample.write_to_file(var,'E:/OCOLOR/data_examples/point_data_samples/ocldb1490697552.28643.APB_processed.csv')
+#WOD_sample = OceanDataProcessor('WOD','E:/OCOLOR/data_examples/point_data_samples/ocldb1490697552.28643.APB.csv',',',1997, 2018, 40, 90, -180, 180, extra={'allowed_titles':['Temperatur']})
+#var = WOD_sample.parse()
+#WOD_sample.write_to_file(var,'E:/OCOLOR/data_examples/point_data_samples/ocldb1490697552.28643.APB_processed3.csv')
+
+#GLODAP_sample = OceanDataProcessor('GLODAP','E:/OCOLOR/data_examples/point_data_samples/GLODAP/GLODAPv2 Arctic Ocean.csv',',',1997, 2018, 40, 90, -180, 180, extra={'allowed_titles':['temperature','depth']})
+#var = GLODAP_sample.parse()
+#GLODAP_sample.write_to_file(var,'E:/OCOLOR/data_examples/point_data_samples/GLODAP/GLODAPv2 Arctic Ocean_processed.csv')
 print("--- Execution time: %s seconds ---" % (time.time() - start_time))
